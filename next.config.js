158d3954e21049e1108abb09f6528451629aeffa
Louis Chan <louiscct922483@gmail.com>
2025-03-17 10:57:32 +0800

    feat: add env and dockerfile

--- next.config.js
+++ next.config.js
@@ -13,6 +13,10 @@ const nextConfig = {
   experimental: {
     optimizeCss: true,
   },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
+  },
 };
 
 module.exports = nextConfig; 
